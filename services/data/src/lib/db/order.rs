d35337e6a87c19fb35b1299afdee91370a8cb88c
Roee Shapira <ro33.sha@gmail.com>
2021-11-09 10:05:14 +0200

    Remove dead code

--- services/data/src/lib/db/order.rs
+++ services/data/src/lib/db/order.rs
@@ -2,8 +2,8 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Order {
-    _id: Option<String>,
+	_id: Option<String>,
 	title: String,
-    ticket_ref: String,
-    date: String,
+	ticket_ref: String,
+	date: String,
 }
