4118d5df6c006ae433fc0586620b3d634a7cc3a5
Roee Shapira <ro33.sha@gmail.com>
2021-11-11 01:19:14 +0200

    Remove unneeded conversion

--- services/data/src/main.rs
+++ services/data/src/main.rs
@@ -76,40 +76,33 @@ async fn main() -> Result<(), ErrorType> {
 	while let Some(Ok((_rx_channel, delivery))) = consumer.next().await {
 		delivery.ack(BasicAckOptions::default()).await?;
 
-		match serde_json::from_slice::<MQMessage<&str>>(&delivery.data) {
+		match serde_json::from_slice::<MQMessage<Vec<&str>>>(&delivery.data) {
 			Ok(message) => {
 				match message.pattern {
 					"get_tickets" => {
-						match serde_json::from_str::<Vec<&str>>(&message.data) {
-							Ok(id_list) => {
-								let filter = if id_list.is_empty() {
-									None
-								} else {
-									Some(doc! { "_id": doc! { "$in": id_list } })
-								};
-
-								let mut cursor = _ticket_collection.find(filter, None).await?;
-								let mut tickets: Vec<Ticket> = Vec::new();
-								while let Some(ticket) = cursor.try_next().await? {
-									tickets.push(ticket);
-								}
-
-								let _confirm = tx
-									.basic_publish(
-										"",
-										"data_queue",
-										BasicPublishOptions::default(),
-										serde_json::to_vec(&tickets)?,
-										BasicProperties::default(),
-									)
-									.await?
-									.await?;
-							}
-							Err(e) => {
-								error!("{} - {}", &e, message.data);
-								continue;
-							}
+						let id_list = message.data;
+						let filter = if id_list.is_empty() {
+							None
+						} else {
+							Some(doc! { "_id": doc! { "$in": id_list } })
 						};
+
+						let mut cursor = _ticket_collection.find(filter, None).await?;
+						let mut tickets: Vec<Ticket> = Vec::new();
+						while let Some(ticket) = cursor.try_next().await? {
+							tickets.push(ticket);
+						}
+
+						let _confirm = tx
+							.basic_publish(
+								"",
+								"data_queue",
+								BasicPublishOptions::default(),
+								serde_json::to_vec(&tickets)?,
+								BasicProperties::default(),
+							)
+							.await?
+							.await?;
 					}
 					other => error!(r#"The pattern "{}" is not supported"#, other),
 				};
